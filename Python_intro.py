983fd47ea74df997db8463c1bf4436b89c155cec
andreaslivova <andreaslivova@gmail.com>
2015-11-13 11:51:21 +0100

    My Django Girls app, first commit

---
+++ Python_intro.py
@@ -0,0 +1,15 @@
+def hi(name):
+	print ("Hi "+name+" !")
+hi("kata")
+
+girls=["Rachel", "Monica", "Phoebe"]
+for name in girls:
+	hi(name)
+
+def vypis():
+	for i in range(5):
+		print (i)
+vypis()
+
+
+
